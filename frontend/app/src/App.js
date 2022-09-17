3bea4dbb74b4815a83a1d92af2634d07444292fc
Janelle Tecson <jantecson30@gmail.com>
2022-09-17 01:07:23 -0400

    testing

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,7 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
+/*
+// all the mail set to empty until given information ?? 
+const front_values = {
+  // mail initially empty
+  mail : [],
+
+  // stats that will be incr/decr based off of the user's performance
+  points : 0,
+  mail_sent : 0,
+  mail_received : 0,
+  // fastest_time : 0,
+
+  // store items
+  store_flower_count : 0,
+  flower_names : [],
+  water : 0,
+
+  // garden
+  garden_flower_count : 0
+  
+};
+
+// retrieving the object values 
+let mail = front_values.mail;
+let points = front_values.points;
+let mail_sent = front_values.mail_sent;
+let mail_received = front_values.mail_received;
+// let fastest_time = front_values.fastest_time;
+
+let store_flower_count = front_values.store_flower_count;
+let flower_names = front_values.flower_names;
+let water = front_values.water;
+
+let garden_flower_count = front_values.garden_flower_count;
+*/
+
+// actual creation of the website
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +59,135 @@ function App() {
       </header>
     </div>
   );
+
+
+  /*
+  return (
+    <div>
+        <nav class="navbar background">
+            <ul class="nav-list">
+                <div class="logo">
+                    <img src=
+"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210420155809/gfg-new-logo.png" />
+                </div>
+                <li><a href="#courses">Courses</a></li>
+                <li><a href="#tutorials">Tutorials</a></li>
+                <li><a href="#jobs">Jobs</a></li>
+                <li><a href='#student'>Student</a></li>
+            </ul>
+
+            <div class="rightNav">
+                <input type="text" name="search" id="search" />
+                <button class="btn btn-sm">Search</button>
+            </div>
+        </nav>
+
+        <section class="section">
+            <div class="box-main">
+                <div class="firstHalf">
+                    <h1 class="text-big">
+                        7 Best Tips To Speed Up Your 
+                        Job Search in 2022
+                    </h1>
+                    <p class="text-small">
+                        Hunting down a relevant job requires 
+                        proper techniques for showcasing your 
+                        potential to the employer. But with 
+                        the advent of COVID-19, it has become 
+                        a bit challenging and competitive to 
+                        reach out for your dream job. Many 
+                        individuals have lost their jobs 
+                        during these times, and on the other 
+                        hand, freshers are facing difficulties
+                        while applying for a new job. But 
+                        there is no need for panic, you can 
+                        change your ways and streamline things 
+                        in a way that you get a proper result.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="box-main">
+                <div class="secondHalf">
+                    <h1 class="text-big" id="program">
+                        JavaScript Tutorial
+                    </h1>
+                    <p class="text-small">
+                        JavaScript is the world most popular 
+                        lightweight, interpreted compiled 
+                        programming language. It is also 
+                        known as scripting  language for
+                        web pages. It is well-known for 
+                        the development of web page many 
+                        non-browser environments also use
+                        it. JavaScript can be used for 
+                        Client-side developments as well 
+                        as Server-side developments.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="box-main">
+                <div class="secondHalf">
+                    <h1 class="text-big" id="program">
+                        Java Programming Language
+                    </h1>
+                    <p class="text-small">
+                        When compared with C++, Java codes 
+                        are generally more maintainable 
+                        because Java does not allow many 
+                        things which may lead to 
+                        bad/inefficient programming if used 
+                        incorrectly. For example, 
+                        non-primitives are always references
+                        in Java. So we cannot pass large
+                        objects (like we can do in C++) to 
+                        functions, we always pass references 
+                        in Java. One more example, since there
+                        are no pointers, bad memory access 
+                        is also not possible. When compared 
+                        with Python, Java kind of fits between
+                        C++ and Python. The programs are written
+                        in Java typically run faster than 
+                        corresponding Python programs and slower 
+                        than C++. Like C++, Java does static 
+                        type checking, but Python does not.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="box-main">
+                <div class="secondHalf">
+                    <h1 class="text-big" id="program">
+                        What is Machine Learning?
+                    </h1>
+                    <p class="text-small">
+                        Machine Learning is the field of study 
+                        that gives computers the capability to 
+                        learn without being explicitly 
+                        programmed. ML is one of the most exciting
+                        technologies that one would have ever 
+                        come across. As it is evident from the 
+                        name, it gives the computer that makes 
+                        it more similar to humans: The ability 
+                        to learn. Machine learning is actively 
+                        being used today, perhaps in many more 
+                        places than one would expect.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <footer className="footer">
+            <p className="text-footer">
+                Copyright ©-All rights are reserved
+            </p>
+        </footer>
+    </div>
+)
+*/
 }
 
 export default App;

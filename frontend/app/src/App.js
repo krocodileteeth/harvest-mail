ee72d9f2972af1b757a4e9f07c3080be75d0a570
Janelle Tecson <jantecson30@gmail.com>
2022-09-17 13:56:05 -0400

    semi working main

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,10 +1,66 @@
 // import logo from './logo.svg';
 import test from './pixelated_ground1.jpg';
 import './App.css';
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// get server_url/mail/status : gives json objects 
+
 
 // actual creation of the website
 function App() {
+  
+  // initialzing data
+  const[data, setData] = useState({data: []});
+  const[isLoading, setIsLoading] = useState(false);
+  const[err, setErr] = useState('');
+
+  const handleClick = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('http://172.26.50.101:5000/mail/status', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          CORS: 'disable'
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetch("/mail/status").then((res) =>
+    res.json().then((data) => {
+        // Setting a data from api
+        // setdata({
+        //     id: data.id,
+        //     sender: data.sender,
+        //     receiver: data.receiver,
+        //     content: data.content,
+        //     read: data.read,
+        //     next_id: data.next_id,
+        //     prev_id: data.prev_id,
+        //     timestamp: data.timestamp
+        //   });
+        document.title = data;
+        console.log(data);
+      })
+    );
+  }, []);
 
  // replace button with calling data to it. event listens to if theres a new email
   return (
@@ -18,7 +74,10 @@ function App() {
       <br/>
       </header>
       <br/>
-      <h4>       
+      <h4>      
+        <div id = "shop">
+          <button> view shop </button>
+        </div>
         <div id = "nav">
           <ul>
             <li><a href = "#inbox">Inbox </a></li>
@@ -32,14 +91,18 @@ function App() {
         <p className = "Mail-content">
           emails go here lmao
           <br/>
-          emails go brrr
-          <br/>
-          emails
-          <br/>
+            <div className = "email-click" >
+            <button>{data.Sender} &emsp; {data.Subject} &emsp; {data.Timestamp}</button>
+            </div>
+            <br/>
+            <div>
+              {err && <h2>{err}</h2>}
+
+              <button onClick={handleClick}>Fetch data</button>
+              {isLoading && <h2>Loading...</h2>}
+            
+            </div>
         </p>
-        <div id = "shop">
-          <button> view shop </button>
-        </div>
       </h4>
       <h5>
         <div>
--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -10,6 +11,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

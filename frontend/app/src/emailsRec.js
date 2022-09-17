3d3c30c4b2e28a68722c599dcbc0d7b37ee89fd9
Janelle Tecson <jantecson30@gmail.com>
2022-09-17 03:54:08 -0400

    Update emailsRec.js

--- frontend/app/src/emailsRec.js
+++ frontend/app/src/emailsRec.js
@@ -1,17 +1,23 @@
-import React from 'react'; 
+import React, { Component } from "react";
+import { Router, Switch, Route } from "react-router-dom";
 
-const ListComponent = (props) => { 
-  
-  return ( 
-    
-    <div className="Component"> 
-    
-      <h1>{props.text}</h1> 
-      
-    </div> 
-    
-  ); 
-  
-}; 
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Products from "./Product/Products";
+import Home from "./Home/Home";
+import history from './history';
 
-export {ListComponent};
+export default class Routes extends Component {
+    render() {
+        return (
+            <Router history={history}>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/About" component={About} />
+                    <Route path="/Contact" component={Contact} />
+                    <Route path="/Products" component={Products} />
+                </Switch>
+            </Router>
+        )
+    }
+}

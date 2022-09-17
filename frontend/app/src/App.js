4cda721d93906a440604520a1baeb99873e95e76
Janelle Tecson <jantecson30@gmail.com>
2022-09-17 03:53:50 -0400

    some ui fixes

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -2,45 +2,9 @@
 import './App.css';
 import React from "react";
 
-/*
-// display this when looking at stats 
-// all the mail set to empty until given information ??
-const front_values = {
-  // mail initially empty
-  mail : [],
-
-  // stats that will be incr/decr based off of the user's performance
-  points : 0,
-  mail_sent : 0,
-  mail_received : 0,
-  // fastest_time : 0,
-
-  // store items
-  store_flower_count : 0,
-  flower_names : [],
-  water : 0,
-
-  // garden
-  garden_flower_count : 0
-  
-};
-
-// retrieving the object values 
-let mail = front_values.mail;
-let points = front_values.points;
-let mail_sent = front_values.mail_sent;
-let mail_received = front_values.mail_received;
-// let fastest_time = front_values.fastest_time;
-
-let store_flower_count = front_values.store_flower_count;
-let flower_names = front_values.flower_names;
-let water = front_values.water;
-
-let garden_flower_count = front_values.garden_flower_count;
-*/
-
 // actual creation of the website
 function App() {
+ // replace button with calling data to it. event listens to if theres a new email
   return (
     <div className="App">
       <header className="App-header">
@@ -50,10 +14,28 @@ function App() {
         <button>Search</button>
       </h1>
       <br/>
-      <h4>
-      &emsp; &emsp; <button>mail</button>
-      </h4>
       </header>
+      <br/>
+      <h4>       
+        <div id = "nav">
+          <ul>
+            <li><a href = "#inbox">Inbox </a></li>
+            <br/>
+            <li><a href="#junk">Junk </a></li>
+            <br/>
+            <li><a href ="#student">Trash</a></li>
+            <br/>
+          </ul>
+        </div>
+        <p className = "Mail-content">
+          emails go here lmao
+          <br/>
+          emails go brrr
+          <br/>
+          emails
+          <br/>
+        </p>
+      </h4>
 
     </div>
   );
---
+++ frontend/app/src/emailsRec.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+const ListComponent = (props) => { 
+  
+  return ( 
+    
+    <div className="Component"> 
+    
+      <h1>{props.text}</h1> 
+      
+    </div> 
+    
+  ); 
+  
+}; 
+
+export {ListComponent};

7a53f982ca02fcfe0a96a0490b0b6c88d4c1162e
Janelle Tecson <jantecson30@gmail.com>
2022-09-17 14:47:55 -0400

    attempt to merge

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,19 +1,58 @@
 // import logo from './logo.svg';
 import test from './pixelated_ground1.jpg';
 import './App.css';
-import React from "react";
-import './Form.js'
-import Container from './Container.js';
+import React, { useState, useEffect } from "react";
+
+// get server_url/mail/status : gives json objects 
 
 
 // actual creation of the website
 function App() {
-  const triggerText = 'Create Email';
-  const onSubmit = (event) => {
-    event.preventDefault(event);
-    console.log(event.target.name.value);
-    console.log(event.target.email.value);
+  
+  // initialzing data
+  const[data, setData] = useState({data: []});
+  const[isLoading, setIsLoading] = useState(false);
+  const[err, setErr] = useState('');
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
   };
+
+  const handleClick = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('http://172.26.50.101:5000/mail/status', {
+        method: 'GET',
+        headers: {
+          ContentType: 'application/json'
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetch("/mail/status").then((res) =>
+    res.json().then((data) => {
+        document.title = data;
+        console.log(data);
+      })
+    );
+  }, []);
+
  // replace button with calling data to it. event listens to if theres a new email
   return (
     <div className="App">
@@ -26,7 +65,17 @@ function App() {
       <br/>
       </header>
       <br/>
-      <h4>       
+      <h4>      
+        <div id = "shop">
+          <button onClick={() => openInNewTab('https://google.com')}> view shop </button>
+        </div>
+        <div className = "email-click">
+              {err && <h2>{err}</h2>}
+
+              <button onClick={handleClick}>refresh emails</button>
+              {isLoading && <h4>Loading...</h4>}
+            
+            </div>
         <div id = "nav">
           <ul>
             <li><a href = "#inbox">Inbox </a></li>
@@ -40,24 +89,17 @@ function App() {
         <p className = "Mail-content">
           emails go here lmao
           <br/>
-          emails go brrr
-          <br/>
-          emails
-          <br/>
         </p>
-        <div id = "shop">
-          <button> view shop </button>
-        </div>
       </h4>
       <h5>
         <div>
         <img src = {test} className = "Ground-Default" alt = "ground" />
         </div>
         <div id = "compose">
-          <button className = "composeButton">compose email</button>
+          <button>compose email</button>
         </div>
-        <Container triggerText={triggerText} onSubmit={onSubmit} />
       </h5>
+
     </div>
   );
 }
